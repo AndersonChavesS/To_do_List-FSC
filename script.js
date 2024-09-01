9ff068568b07f255198d1448b12701f3820977fe
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-09-01 17:15:25 -0300

    feat: added save functionality to local storage

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ const handleAddTask = () => {
   taskItemContainer.appendChild(deleteItem);
   tasksContainer.appendChild(taskItemContainer);
   inputElement.value = '';
+  updateLocalStorage();
 };
 
 const handleDeleteClick = (taskItemContainer, taskContent) => {
@@ -38,6 +39,7 @@ const handleDeleteClick = (taskItemContainer, taskContent) => {
       taskItemContainer.remove();
     }
   }
+  updateLocalStorage();
 };
 
 const handleClick = (taskContent) => {
@@ -48,6 +50,46 @@ const handleClick = (taskContent) => {
       task.firstChild.classList.toggle('completed');
     }
   }
+  updateLocalStorage();
+};
+
+const updateLocalStorage = () => {
+  const tasks = tasksContainer.childNodes;
+
+  const localStorageTasks = [...tasks].map((task) => {
+    const content = task.firstChild;
+    const IsCompleted = content.classList.contains('completed');
+    return { description: content.innerText, IsCompleted };
+  });
+  localStorage.setItem('tasks', JSON.stringify(localStorageTasks));
+};
+
+const refreshTasksUsingLocalStorage = () => {
+  const tasksFromLocalStorage = JSON.parse(localStorage.getItem('tasks'));
+
+  if (!tasksFromLocalStorage) return;
+
+  for (const task of tasksFromLocalStorage) {
+    const taskItemContainer = document.createElement('div');
+    taskItemContainer.classList.add('task-item');
+
+    const taskContent = document.createElement('p');
+    taskContent.innerText = task.description;
+    if (task.IsCompleted) {
+      taskContent.classList.add('completed');
+    }
+    taskContent.addEventListener('click', () => handleClick(taskContent));
+
+    const deleteItem = document.createElement('i');
+    deleteItem.classList.add('far');
+    deleteItem.classList.add('fa-trash-alt');
+    deleteItem.addEventListener('click', () =>
+      handleDeleteClick(taskItemContainer, taskContent)
+    );
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+    tasksContainer.appendChild(taskItemContainer);
+  }
 };
 
 const handleInputChange = () => {
@@ -57,5 +99,6 @@ const handleInputChange = () => {
   }
 };
 // Events
+refreshTasksUsingLocalStorage();
 addTaskButton.addEventListener('click', () => handleAddTask());
 inputElement.addEventListener('change', () => handleInputChange());
