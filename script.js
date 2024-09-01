6916417560d7909f50e516151168994103c7f0eb
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-09-01 16:37:12 -0300

    feat: added function to delete item

--- script.js
+++ script.js
@@ -15,16 +15,41 @@ const handleAddTask = () => {
 
   const taskContent = document.createElement('p');
   taskContent.innerText = inputElement.value;
+  taskContent.addEventListener('click', () => handleClick(taskContent));
 
   const deleteItem = document.createElement('i');
   deleteItem.classList.add('far');
   deleteItem.classList.add('fa-trash-alt');
+  deleteItem.addEventListener('click', () =>
+    handleDeleteClick(taskItemContainer, taskContent)
+  );
 
   taskItemContainer.appendChild(taskContent);
   taskItemContainer.appendChild(deleteItem);
   tasksContainer.appendChild(taskItemContainer);
   inputElement.value = '';
 };
+
+const handleDeleteClick = (taskItemContainer, taskContent) => {
+  const tasks = tasksContainer.childNodes;
+  for (const task of tasks) {
+    const currentTaskBeingClicked = task.firstChild.isSameNode(taskContent);
+    if (currentTaskBeingClicked) {
+      taskItemContainer.remove();
+    }
+  }
+};
+
+const handleClick = (taskContent) => {
+  const tasks = tasksContainer.childNodes;
+  for (const task of tasks) {
+    const currentTaskBeingClicked = task.firstChild.isSameNode(taskContent);
+    if (currentTaskBeingClicked) {
+      task.firstChild.classList.toggle('completed');
+    }
+  }
+};
+
 const handleInputChange = () => {
   const inputIsValid = validateInput();
   if (inputIsValid) {
