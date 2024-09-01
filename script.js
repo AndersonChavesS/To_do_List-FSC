11afe03336327956b7196eb73956fc6607d86e9e
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-09-01 16:03:57 -0300

    feat: add tasks on tasks container

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const addTaskButton = document.querySelector('.new-task-button');
 const inputElement = document.querySelector('.new-task-input');
+const tasksContainer = document.querySelector('.tasks-container');
 
-const validateInput = () => inputElement.value.trim().lenght > 0;
+const validateInput = () => inputElement.value.trim().length > 0;
+
 const handleAddTask = () => {
   const inputIsValid = validateInput();
   if (!inputIsValid) {
     return inputElement.classList.add('error');
   }
+
+  const taskItemContainer = document.createElement('div');
+  taskItemContainer.classList.add('task-item');
+
+  const taskContent = document.createElement('p');
+  taskContent.innerText = inputElement.value;
+
+  const deleteItem = document.createElement('i');
+  deleteItem.classList.add('far');
+  deleteItem.classList.add('fa-trash-alt');
+
+  taskItemContainer.appendChild(taskContent);
+  taskItemContainer.appendChild(deleteItem);
+  tasksContainer.appendChild(taskItemContainer);
+  inputElement.value = '';
 };
 const handleInputChange = () => {
   const inputIsValid = validateInput();
@@ -14,7 +31,6 @@ const handleInputChange = () => {
     return inputElement.classList.remove('error');
   }
 };
-
 // Events
 addTaskButton.addEventListener('click', () => handleAddTask());
 inputElement.addEventListener('change', () => handleInputChange());
